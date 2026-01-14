07135c4d5376d3ff60a1e0143759ecd3dcc04581
Nguyễn Đình Thái <dinhthai0312@gmail.com>
2026-01-14 22:59:13 +0700

    chore: testing

--- test.py
+++ test.py
@@ -1,44 +1,13 @@
 from zalo_client import ZaloClient
 import json
-secret_key_b64 = 'hdqA2PcowjsqGRrtLtb2FA=='
-cookie_string = "ozi=2000.SSZzejyD2DyiZwEqqGn1pJ75lh39JHN1E8Yy_zm36zbwrAxraqyOtpIUfVUMIX7VCj6bz9865zatrQNyD3ar.1; _ga_1J0YGQPT22=GS1.1.1743267239.1.1.1743267278.21.0.0; _gcl_au=1.1.1210773121.1762247361; _fbp=fb.1.1762247361146.837283859710473447; __zi=3000.SSZzejyD2DyiZwEqqGn1pJ75lh39JHN1E8Yy_zm36zbxrAxraayOt3EUhlQGGHEMDP6YkfP75f8rcQUtDG.1; __zi-legacy=3000.SSZzejyD2DyiZwEqqGn1pJ75lh39JHN1E8Yy_zm36zbxrAxraayOt3EUhlQGGHEMDP6YkfP75f8rcQUtDG.1; _ga_907M127EPP=GS2.1.s1766142174$o6$g1$t1766142174$j60$l0$h0; _ga_YT9TMXZYV9=GS2.1.s1767670825$o9$g0$t1767670825$j60$l0$h0; zoaw_sek=QkLN.1968800208.2.8wLFSLG-2NI4lr01L3u9T5G-2NHKFWbqLGMDA3q-2NG; zoaw_type=0; _ga_NVN38N77J3=GS2.2.s1767670840$o4$g1$t1767670845$j55$l0$h0; _ga_WSPJQT0ZH1=GS2.1.s1767670858$o3$g1$t1767670880$j38$l0$h0; _ga_E63JS7SPBL=GS2.1.s1767670834$o5$g1$t1767670883$j11$l0$h0; _ga=GA1.2.759643980.1743071453; _gid=GA1.2.1733340641.1767856264; _ga_RYD7END4JE=GS2.2.s1767856264$o47$g1$t1767856265$j59$l0$h0; _ga_YS1V643LGV=GS2.1.s1767856264$o48$g0$t1767856265$j59$l0$h0; _zlang=vn; _ga_3EM8ZPYYN3=GS2.2.s1767856272$o41$g0$t1767856272$j60$l0$h0; zpsid=zew_.355636788.155.DPQiPxWTvLUCYzT2j1tbLyrkbsIDADDaZYhTRwDJuMoc04BmkR-z2D4TvLS; zpw_sek=ZuUG.355636788.a0.-iRXdssXCFTPDmBYJA4mznQ3LPnFcnAV2T1nnJt2Nu07qa6mFzD4knd30h4id0tL4Bf-yrQ_BL3Ae9ElCiWmzm; app.event.zalo.me=616744305790528006"
-client = ZaloClient("", '')
-
-# data = "57i4rgZDfv2WWov6Wf/tnqAhbhEqQBABBMyjYlgSevKXgqrA84aUF2byHLx/cVr5NwTfs1o3O+4VAFV1b3K4ah8ONr/jIJuh5SgZzJHcnrzxAnLgO7nd4uiAlx7hXex3nWCWZ670jz7SQlDKbJ66dsHPtNp8o8DXxuKBcfmcT+tw9D1QX0Frk0PLnn/NyfMtuPR8YoVQZ/AzV8E++pkC2w=="
-
-
-# decode = client.decodeAES(data)
-# print(decode)
+secret_key_b64 = 'YU9bjfEYcbJPvJozl4s4OQ=='
+cookie_string = "ozi=2000.SSZzejyD2DyiZwEqqGn1pJ75lh39JHN1E8Yy_zm36zbwrAxraqyOtpIUfVUMIX7VCj6bz9865zatrQNyD3ar.1; _ga_1J0YGQPT22=GS1.1.1743267239.1.1.1743267278.21.0.0; _gcl_au=1.1.1210773121.1762247361; _fbp=fb.1.1762247361146.837283859710473447; __zi=3000.SSZzejyD2DyiZwEqqGn1pJ75lh39JHN1E8Yy_zm36zbxrAxraayOt3EUhlQGGHEMDP6YkfP75f8rcQUtDG.1; __zi-legacy=3000.SSZzejyD2DyiZwEqqGn1pJ75lh39JHN1E8Yy_zm36zbxrAxraayOt3EUhlQGGHEMDP6YkfP75f8rcQUtDG.1; _ga_NVN38N77J3=GS2.2.s1767670840$o4$g1$t1767670845$j55$l0$h0; _ga_WSPJQT0ZH1=GS2.1.s1767670858$o3$g1$t1767670880$j38$l0$h0; _ga_E63JS7SPBL=GS2.1.s1767670834$o5$g1$t1767670883$j11$l0$h0; _ga_907M127EPP=GS2.1.s1767944044$o7$g1$t1767944083$j21$l0$h0; _gid=GA1.2.1626715681.1768391792; _zlang=vn; app.event.zalo.me=616744305790528006; _ga_3EM8ZPYYN3=GS2.2.s1768397818$o53$g0$t1768397818$j60$l0$h0; zpsid=u5s7.355636788.168.5d3tEfg1nykk5_NUbe77o-_ojVYljF7uhBR_yvkrgf24d61icuPRhFE1nyi; zpw_sek=uZ26.355636788.a0.vPy_gtoh8Vr8FHFeNQiX_GU9H9PUaGEaAP93bnhkU9uUuN6BDiHRaKZO5R8jbXpV0QeLa2UrF5hRgYPX6y8X_G; _ga_YT9TMXZYV9=GS2.1.s1768403726$o15$g0$t1768403726$j60$l0$h0; _ga=GA1.1.759643980.1743071453; _ga_YS1V643LGV=GS2.1.s1768403733$o62$g0$t1768403734$j59$l0$h0; _ga_RYD7END4JE=GS2.2.s1768403734$o60$g1$t1768403734$j60$l0$h0"
+client = ZaloClient(secret_key_b64, cookie_string)
+data = "yaG1UhO2SDk441T5zEorynHCqavB440lawZx5mJ4bG6ITJgDkJysbZfQAekBgHV4kzgsSHR0wcX0xXH5o3Ft9xnKWRatPFyh0UKOcMqdxV0Y7oiuDbrigSDLT6qsoUYQeZ8Dpw4SS48djEuqwz68jbBKECGAb79waIMsFEvYqliYea6ByZAFXegumThEVmePZ4vG4558wqNFhU7Ru+WlvQ=="
+decode = client.decodeAES(data)
+print(decode)
 # list_friend = client.getRecommendedFriendsV2()
 # print(list_friend)
-# Ví dụ gửi tin nhắn cho cá nhân
-# try:
-#     result = client.sendZText(
-#         client_id=1766136198020,
-#         to_id="3990347544143465685",
-#         message="Xin chào! Đây là tin nhắn tự động!",
-#         is_group=False,
-#         debug=True  # Bật debug để xem thông tin chi tiết
-#     )
-#     print("Kết quả:", result)
-# except Exception as e:
-#     print("Lỗi:", e)
-
-# # Ví dụ gửi tin nhắn vào group
-# try:
-#     result = client.sendZText(
-#         to_id="GROUP_ID_HERE",
-#         message="Hello group!",
-#         is_group=True,
-#         visibility=0  # 0 = hiển thị cho tất cả
-#     )
-#     print("Gửi tin nhắn group thành công:", result)
-# except Exception as e:
-#     print("Lỗi:", e)
-
-# UID của người cần lấy (ví dụ từ log của bạn)
-# target_uid = "7921649594554906494"
-
 # # Gọi API
 # try:
 #     # friend_pversion_map: "UID_0" để lấy thông tin mới nhất
@@ -63,11 +32,11 @@ client = ZaloClient("", '')
 # print("Gửi thành công:", json.dumps(resp, indent=2, ensure_ascii=False))
 
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
     # 1. Khởi tạo
     
     # 2. Đăng nhập QR (Lấy Cookie id.zalo.me & Session Init)
-    user_info = client.wait_for_qr_login()
+    # user_info = client.wait_for_qr_login()
     
     # if user_info:
     #     # 3. Hoàn tất đăng nhập (Lấy Secret Key cho chat.zalo.me)
